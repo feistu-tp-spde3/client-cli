dea6ecbf7b9878d494f4deac0392e6b18d45cbb9
Michal Malík <noonesvk@gmail.com>
2019-01-13 20:06:53 +0100

    Remove old declarations from mysql class

--- src/MySqlJdbcConnector.hpp
+++ src/MySqlJdbcConnector.hpp
@@ -21,21 +21,10 @@ private:
 	sql::Driver *m_driver;
 	std::unique_ptr<sql::Connection> m_connection;
 
-	std::vector<std::string> getColumns(const std::string &table_name, bool auto_increment);
-	std::string formatInsertStatement(const std::string &table_name, const std::vector<std::string> &columns);
-
 public:
     MySqlJdbcConnector();
 
 	bool connect(const std::string &xml_db_config);
-
-	/*
-	*    Inserts values into the given table and returns the number of rows affected
-	* NOTE:
-	*    Values have to be in the order in which we are about to store them into the database
-	*    eg. table columns: name, somenumber, email ... values: "Big Chungus", 420, "idk[at]gmail.com" ...
-	*/
-	int insert(const std::string &table_name, const std::vector<std::string> &values, bool auto_increment = false);
 
 	std::unique_ptr<sql::Statement> createStatement();
 
